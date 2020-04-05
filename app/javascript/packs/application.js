450e8c3e66758b44c3cec3ea0e39d38aaf209fc8
大沼豪 <abdminl555@au.com>
2020-04-05 10:03:36 +0900

    画像の複数投稿、削除（画像の編集機能は未）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require('create.js.erb')
-require('destroy.js.erb')
+require("active_storage.js.erb")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
